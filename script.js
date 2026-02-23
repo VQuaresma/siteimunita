cba899530be464870b4874222aadaa4c422b6c22
Vitor Quaresma <vitorquaresmadasilva@gmail.com>
2026-02-23 17:15:13 -0300

    criação da section de médicos que trabalham na clinica + animações

--- script.js
+++ script.js
@@ -149,3 +149,19 @@ menuLinks.forEach(link => {
         }
     });
 });
+
+
+const observerOptions = {
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.remove('opacity-0', 'translate-y-20', 'translate-y-10');
+                entry.target.classList.add('opacity-100', 'translate-y-0');
+            }
+        });
+    }, observerOptions);
+
+    document.querySelectorAll('.reveal').forEach((el) => observer.observe(el));
